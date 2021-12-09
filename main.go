6ed6e33af59dd6db0c159e6fa4c4e0d2f950fe06
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2021-12-09 21:22:11 +0100

    :sparkles: Adds flag bullshit

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"time"
 )
 
 func main() {
-	bot, err := discordgo.New("Bot OTE4NDU0MTE0NjgwMTkzMDM2.YbHfDA.3V0vrsmcOjgK6jOyT8KFnIeWVFE")
+	token := flag.String("token", "", "Bot token")
+	nickname := flag.String("nickname", "LauraFuckMePls", "Tulek's new nickname")
+	sleepInterval := flag.Int("sleep", 10, "Sleep interval in seconds")
+	flag.Parse()
+
+	if *token == "" {
+		fmt.Errorf("Please provide a token\n")
+		return
+	}
+
+	if len(*nickname) > 25 {
+		fmt.Errorf("Nickname is too long\n")
+		return
+	}
+
+	bot, err := discordgo.New("Bot " + *token)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	bot.LogLevel = discordgo.LogInformational
+	bot.LogLevel = discordgo.LogWarning
 
 	err = bot.Open()
 
@@ -27,12 +43,12 @@ func main() {
 	for {
 		fmt.Println("Doing the nasty...")
 
-		err := bot.GuildMemberNickname("770229888195493888", "533331780397301790", "Potato LauraPleaseFuckMe")
+		err := bot.GuildMemberNickname("770229888195493888", "533331780397301790", "Potato "+*nickname)
 		if err != nil {
 			fmt.Println(err.Error())
 			continue
 		}
 
-		time.Sleep(time.Second * 10)
+		time.Sleep(time.Second * time.Duration(*sleepInterval))
 	}
 }
